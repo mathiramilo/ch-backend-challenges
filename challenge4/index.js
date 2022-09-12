5c0ab3d3a18e88028b73b0134705b355cbfe2646
mathias.ramilo <mathias.ramilo@fing.edu.uy>
2022-09-12 17:47:44 -0300

    Challenge 4 - API RESTfull

--- challenge4/index.js
+++ challenge4/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const path = require('path')
+const apiRoutes = require('./routes/index')
+const logger = require('./middlewares/logger')
 
 const PORT = process.env.PORT || 8080
 const app = express()
 
 // Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(logger)
 
 // Routes
+app.use('/api', apiRoutes)
+app.get('*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/public/pages/404.html'))
+})
 
 const server = app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`)
