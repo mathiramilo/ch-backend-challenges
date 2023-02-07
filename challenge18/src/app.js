38279ea1d6076bcf1de098d5b1a9b69a386f0721
Mathias Ramilo <mathiramilo2290@gmail.com>
2023-02-07 16:33:38 -0300

    Challenge 18 - API Documentation with Swagger

--- challenge18/src/app.js
+++ challenge18/src/app.js
@@ -1,9 +1,13 @@
 // Imports
 const path = require('path')
 const express = require('express')
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+
 const loggerMiddleware = require('./middlewares/logger')
 const errorMiddleware = require('./middlewares/error')
 const logger = require('./utils/logger.utils')
+
 const apiRoutes = require('./routes/api/apiRoutes')
 const webRoutes = require('./routes/web/webRoutes')
 
@@ -14,6 +18,22 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('src/public'))
 app.use(loggerMiddleware)
+
+// Swagger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'CH Backend Challenges API',
+      description: 'Rest API for the Coderhouse backend course challenges',
+      version: '1.0.0'
+    }
+  },
+  apis: [path.join(__dirname, '../docs/**/*.yaml')]
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+app.use('/apidocs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 // Routes
 app.use(webRoutes)
--- challenge18/src/config.js
+++ challenge18/src/config.js
@@ -14,7 +14,9 @@ const args = yargs(process.argv.slice(2))
 const config = {
   MODE: args.mode,
   PORT: process.env.PORT || 8080,
-  MONGO_URI: process.env.MONGO_URI
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRE: process.env.JWT_EXPIRE
 }
 
 module.exports = config
